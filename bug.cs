90cbe8b32fb1a553dbf0a40336ed9f8d7b37b786
BugHunter-X <askzenai@gmail.com>
2025-01-20 13:16:05 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,8 @@
+public class ExampleClass{
+    public int MyProperty { get; set; }
+
+    public void MyMethod(){
+        // Accessing a property that hasn't been initialized.
+        int value = MyProperty * 2; 
+    }
+}
